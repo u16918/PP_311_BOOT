fda255be113e347b04e96b200d1023bb84237de6
Александр <u16918@yandex.ru>
2022-10-15 00:16:41 +0700

    commit2

---
+++ src/main/java/ru/spring/web/Pp311SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp311SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spring/web/Pp311SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
